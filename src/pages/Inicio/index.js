34f251e51142589a28223c95c39a3b698b79bd44
Cléber Severo <clebersevero1998@gmail.com>
2023-06-16 12:03:40 -0300

    feat: reading the data json to create dynamic cards

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -4,6 +4,9 @@ import Card from 'components/Card';
 import Rodape from 'components/Rodape';
 import Titulo from 'components/Titulo';
 import React from 'react';
+import videos from 'json/db.json'
+
+import styles from './Inicio.module.css'
 
 function Inicio() {
   return (
@@ -13,7 +16,11 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
-      <Card id='1' titulo='gato bonifácio' capa="C:\Users\clebe\dev\alura\Cursos\React praticando React com js\cinetag\public\images\Imagem-Dunga.png"/>
+      <section className={styles.container}>
+        { videos.map( video => (
+          <Card {...video} key={video.id} />
+        ) ) }
+      </section>
       <Rodape />
     </>
 
