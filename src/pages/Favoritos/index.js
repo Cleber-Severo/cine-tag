d0f862eb7471f2db11708c80d521c49989f050e7
Cléber Severo <clebersevero1998@gmail.com>
2023-06-16 12:49:11 -0300

    feat: favorites page components

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,11 +1,21 @@
+import Banner from 'components/Banner'
 import styles from './Favoritos.module.css'
 
 import React from 'react'
+import Titulo from 'components/Titulo'
+import Card from 'components/Card'
 
 function Favoritos() {
   return (
     <>
+        <Banner imagem="favoritos"/>
+        <Titulo>
+            <h1>Meus Favoritos</h1>
+        </Titulo>
 
+        <section className={styles.container} >
+              <Card id='1' titulo='Gato Bonifacio' capa='cinetag\public\images\Imagem-Dunga.png' />
+        </section>
     </>
   )
 }
