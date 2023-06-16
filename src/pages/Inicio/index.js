1a2d7a09b22584dfc81fdd931b922931facf39af
Cléber Severo <clebersevero1998@gmail.com>
2023-06-16 11:52:53 -0300

    feat: creating card component

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,5 +1,6 @@
 import Banner from 'components/Banner';
 import Cabecalho from 'components/Cabecalho';
+import Card from 'components/Card';
 import Rodape from 'components/Rodape';
 import Titulo from 'components/Titulo';
 import React from 'react';
@@ -12,6 +13,7 @@ function Inicio() {
       <Titulo>
         <h1>Um lugar para guardar seus vídeos e filmes!</h1>
       </Titulo>
+      <Card id='1' titulo='gato bonifácio' capa="C:\Users\clebe\dev\alura\Cursos\React praticando React com js\cinetag\public\images\Imagem-Dunga.png"/>
       <Rodape />
     </>
 
