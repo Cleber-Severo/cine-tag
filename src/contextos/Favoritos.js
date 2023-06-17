4b85a71b4aa6df2b88319cec69a8a92ed84676a4
Cléber Severo <clebersevero1998@gmail.com>
2023-06-17 09:00:16 -0300

    feat: cumtom hook useFavorito to add new favorite movies

--- src/contextos/Favoritos.js
+++ src/contextos/Favoritos.js
@@ -1,9 +1,9 @@
-import { createContext, useState } from "react"
+import { createContext, useContext, useState } from "react"
 
 export const FavoritosContext = createContext();
 FavoritosContext.displayName = "Favoritos"
 
-export default function FavoritosProvider ([children]) {
+export default function FavoritosProvider ({children}) {
     const [favorito, setFavorito] = useState([]);
 
     return (
@@ -11,4 +11,24 @@ export default function FavoritosProvider ([children]) {
             {children}
         </FavoritosContext.Provider>
     )
+}
+
+export function useFavoritoContext() {
+    const { favorito, setFavorito} = useContext(FavoritosContext);
+
+    function adicionarFavorito(novoFavorito) {
+        const favoritoRepetido = favorito.some( item => item.id === novoFavorito.id )
+
+        let novaLista = [...favorito];
+
+        if(!favoritoRepetido) {
+            novaLista.push(novoFavorito);
+            return setFavorito(novaLista)
+        }
+
+        novaLista.slice(novaLista.indexOf(novoFavorito), 1);
+        return setFavorito(novaLista);
+    }
+
+    return { favorito, adicionarFavorito }
 }
