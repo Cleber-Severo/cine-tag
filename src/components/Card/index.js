e2b60141c64fb043a18c0be648d500000af489ed
Cléber Severo <clebersevero1998@gmail.com>
2023-06-17 09:55:52 -0300

    feat: addin the favorites using the favortiteContext and switching the fav icon

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
 import styles from './Card.module.css';
-import iconeFavoritar from './favoritar.png'
+import iconeFavoritar from './favoritar.png';
+import iconeDesvavoritar from './desfavoritar.png';
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Card({id, titulo, capa}) {
+  const { favorito, adicionarFavorito } = useFavoritoContext();
+  const ehFavorito = favorito.some( fav => fav.id === id)
+  const icone = !ehFavorito ? iconeFavoritar : iconeDesvavoritar;
+
   return (
     <div className={styles.container} >
         <img src={capa} alt={titulo} className={styles.capa} />
         <h2>{titulo}</h2>
-        <img src={iconeFavoritar} alt="Favoritar Filme" className={styles.favoritar} />
+        <img 
+          src={icone} 
+          alt="Favoritar Filme" 
+          className={styles.favoritar} 
+          onClick={() => adicionarFavorito({ id, titulo, capa })}
+        />
     </div>
   )
 }
