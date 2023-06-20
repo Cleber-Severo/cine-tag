79168f521096486fb7cf634ff5c5086db7d4305f
Cléber Severo <clebersevero1998@gmail.com>
2023-06-19 22:43:45 -0300

    feat: nested routes

--- src/routes.js
+++ src/routes.js
@@ -7,22 +7,19 @@ import Container from "components/Container";
 import FavoritosProvider from "contextos/Favoritos";
 import Player from "pages/Player";
 import NaoEncontrada from "pages/NaoEncontrada";
+import PaginaBase from "pages/PaginaBase";
 
 function AppRoutes() {
     return(
         <BrowserRouter>
-            <Cabecalho />
-            <Container>
-                <FavoritosProvider>
-                    <Routes>
-                        <Route path="/" element={<Inicio />} ></Route>
-                        <Route path="/favoritos" element={<Favoritos/>} ></Route>
-                        <Route path="/:id" element={<Player/>} ></Route>
-                        <Route path="*" element={<NaoEncontrada/>}></Route>
-                    </Routes>
-                </FavoritosProvider>
-            </Container>
-            <Rodape />
+            <Routes>
+                <Route path="/" element={<PaginaBase />}>
+                    <Route index element={<Inicio />} ></Route>
+                    <Route path="favoritos" element={<Favoritos/>} ></Route>
+                    <Route path=":id" element={<Player/>} ></Route>
+                    <Route path="*" element={<NaoEncontrada/>}></Route>
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
